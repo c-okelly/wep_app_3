ccd2419292d9cc40d60649605c80ad4df99633bb
cokelly <okellyconor@gmail.com>
2016-04-30 22:43:11 +0100

    Added dbconfig file. Finished index.php page

--- footer.php
+++ footer.php
@@ -3,6 +3,52 @@
   <title>Footer</title>
  </head>
  <body>
- <?php echo '<p>hi guys. Hello World</p>'; ?> 
+<div class="container footer"> <!-- Footer -->
+            <div class="row">
+                <div class="seven columns">
+                    <footer class="small_footer">
+                        This Website was built using a Skeleton Framework
+                        <br> Built by Conor O'Kelly
+                        <br> <a class="email" href="mailto:C10305243@dit.ie">Contact: Myemail@no.com</a>
+                        <section>The current date and time is:  
+                            <script type="text/javascript">
+                            var currentDate = new Date()
+                            var month = currentDate.getMonth() + 1
+                            var day = currentDate.getDate()
+                            var year = currentDate.getFullYear()
+                            document.write(month + "/" + day + "/" + year)
+                            </script>
+                            <script type="text/javascript">
+                                var currentTime = new Date()
+                                var hour = currentTime.getHours()
+                                var minute = currentTime.getMinutes()
+                                if (minute < 10 ){ 
+                                    minute = "0" + minute
+                                }
+                                document.write(hour + ":" + minute)
+                                if (hour > 11 ){ 
+                                    document.write(" PM")
+                                } else {
+                                    document.write(" AM")
+                                }
+                                
+                            </script>
+                            </section>
+                    </footer>
+                </div>
+                
+                <div class="five columns">
+                    <section class="center">
+                        <div class="social small_social">
+                        Wana to get Social? <br>
+                            <a href="https://www.facebook.com/bicycletouring" target="_blank"><span> <img src="images/Social/facebook42.jpg" class="rotate" title="Facebook link" alt="link"></span></a>
+                            <a href="https://twitter.com/hashtag/cycletouring" target="_blank"><span> <img src="images/Social/twitter42.png" class="rotate" title="Twitter link" height="40" width="40" alt="link"></span></a>
+                            <a href="https://www.youtube.com/user/globalcyclingnetwork" target="_blank"><span> <img src="images/Social/youtube42.png" class="rotate" title="Youtube link" height="40" width="40" alt="link"></span></a>
+                            <a href="https://ie.linkedin.com/pub/conor-o-kelly/68/a67/658" target="_blank"><span> <img src="images/Social/linkedin42.png" class="rotate" title="LinkedIn link" height="40" width="40" alt="link"></span></a>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div><!-- Footer end -->
  </body>
 </html>
--- index.php
+++ index.php
@@ -37,6 +37,54 @@
      
      
         <!--            Main page content        -->
+     <div>
+         <p class="center underlined">
+            Here is a list and discription of all the produts that are currently being offered by Classic models!
+         </p>
+
+     </div>
+
+     
+<!--    Connect to database       -->
+     <?php        
+//        require_once "dbconfig.php";
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "classicmodels";
+     
+     try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+         
+        $sql = "SELECT * FROM productlines;";
+        $query = $conn->query($sql);
+        
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            $product = $row['productLine'];
+            $descrip = $row['textDescription'];
+            // Print out div of relevant information
+            echo '<div> 
+        <p class="center underlined">', $product ,' </p>
+         <p class="index_data">
+         ',$descrip,'
+         </p>
+     </div>
+            ';
+            }
+
+//        echo "New records created successfully";
+     }
+     catch (PDOException $e)
+        {
+        echo "Error: " . $e->getMessage();
+        }
+     // Kill connection
+     $conn = null;
+         
+     ?>
      
      
         <!--            Include footer for page        -->
