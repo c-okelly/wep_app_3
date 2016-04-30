4c87e38ac6dc8bdca30c5f70b37bc30e9c5fddf0
cokelly <okellyconor@gmail.com>
2016-04-30 16:42:15 +0100

    Added old website info and udpated nav and index phage

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+ <head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <meta charset="utf-8">
+        <title>Home Page</title>
+        <meta name="Weather Forecast!" content="">
+        <meta name="Conor O'Kelly" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <!-- FONT
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+      <!-- CSS
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <link rel="stylesheet" href="css/normalize.css" type="text/css">
+        <link rel="stylesheet" href="css/skeleton.css"  type="text/css">
+        <link rel="stylesheet" href="css/styles.css" type="text/css">
+
+    <!-- JavaScript
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+
+      <!-- Icon
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <link rel="icon" type="image/png" href="images/Icon.jpg">
+    </head>
+ <body class="contact">
+        <!--           Include banner and nav          -->
+            <?php include "Nav.php"; ?>    
+ </body>
+</html>
---
+++ nav.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        
+              <!-- CSS
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <link rel="stylesheet" href="css/normalize.css" type="text/css">
+        <link rel="stylesheet" href="css/skeleton.css"  type="text/css">
+        <link rel="stylesheet" href="css/styles.css" type="text/css">
+    
+    </head>
+    <body class="contact">
+        <!-- Primary Page Layout  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <div class="container banner">        
+            <div class="row"> <!-- Banner -->
+                <div class="four columns banner_text" >
+                    <section class="center">
+                        Classic car Models<br>
+                    </section>
+                </div>
+            </div> <!-- Banner end -->
+        </div>
+     
+        <div class="container nav"><!-- Nav -->  
+            <div class="row"> 
+                <div class="twelve columns"> 
+                    <nav>
+                        <ul>
+                            <li> <a href="index.php">Home</a></li>
+                            <li><a href="Product.php">Products</a>
+                            <li><a  href="Customers.php">Customers</a>
+                            <li><a href="Order.php">Orders</a>
+                        </ul>
+                        
+                        <div class="mobile_nav">
+                            <select onchange="if (this.value) window.location.href=this.value">
+                                <option value="" selected="selected">Menu</option> 
+                                <option value="index.html">Home</option> 
+                                <option value="Products.php">Products</option>
+                                <option value="Customers.php">Customers</option>
+                                <option value="Order.php">Orders</option>
+                            </select>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div><!-- Nav end -->  
+    </body>
+</html>
